c0f4cd4e9b9f332d1e79e6b58ac9f0a62ea1ae1e
jpromanonet <programming@jpromano.net>
2020-09-01 20:50:58 -0600

    edad

--- forms/conn.php
+++ forms/conn.php
@@ -1,8 +1,8 @@
 <?php
-$dbhost	= "192.99.21.118";	   
-$dbuser	= "masinver_admin";		  
-$dbpass	= "masinver2019";		     
-$dbname	= "masinver_freeApps";
+$dbhost	= "localhost";
+$dbuser	= "root";
+$dbpass	= "1234";
+$dbname	= "formularios";
 
 $conn = new mysqli($dbhost,$dbuser,$dbpass,$dbname) or die(mysqli_error($conn));
 ?>
