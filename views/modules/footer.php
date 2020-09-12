28c4c2c8c9a9c3ff601b1933054d3f3124f12544
jpromanonet <programming@jpromano.net>
2020-09-12 18:54:08 -0300

    New design

---
+++ views/modules/footer.php
@@ -0,0 +1,24 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+  <div class="footer-top">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-6">
+          <h3>Seguinos en...</h3>
+        </div>
+      </div>
+
+      <div class="social-links">
+        <a href="https://twitter.com/thestartingdev" target="_blank" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="https://www.instagram.com/thestartingdev/" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="https://www.linkedin.com/company/the-starting-dev" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        <a href="https://github.com/thestartingdev" target="_blank" class="github"><i class="bx bxl-github"></i></a>
+        <a href="https://www.youtube.com/channel/UC09pnxn0b-dVNS0rWotnWQQ" target="_blank" class="youtube"><i class="bx bxl-youtube"></i></a>
+      </div>
+      <br>
+      <br>
+      <h6><a href="./terminosCondiciones.html">Terminos y condiciones</a> | <a href="./politicaPrivacidad.html"> Politica de privacidad</a></h6>
+    </div>
+  </div>
+</footer>
+<!-- End Footer -->
---
+++ views/modules/navbar.php
@@ -0,0 +1,27 @@
+  <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top">
+      <div class="container d-flex align-items-center">
+        <h1 class="logo mr-auto"><a href="./index.php">The Starting Dev</a></h1>
+        <nav class="nav-menu d-none d-lg-block">
+          <ul>
+            <li class="active"><a href="./index.php">Inicio</a></li>
+            <li><a href="./views/acerca.php">Acerca de</a></li>
+            <li class="drop-down">
+              <a  href="#">Información</a>
+              <ul>
+                <li><a href="./views/consejos.php">Consejos Dev</a></li>
+                <li><a href="./views/recursos.php">Recursos gratis</a></li>
+                <li ><a href="./views/inscripcion.php">Inscripción</a></li>
+                <li><a href="./views/voluntario.php">Ser Voluntario</a></li>
+                <li><a href="./views/ideas.php">Aportar Ideas</a></li>
+                <li><a href="./views/conducta.php">Codigo de Conducta</a></li>
+                <li><a href="./views/roadmap.php">Roadmap / Programa</a></li>
+              </ul>
+            </li>
+            <li><a href="./views/contacto.php">Contacto</a></li>
+            <li><a href="/campus/"><b>Campus</b></a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <!-- End Header -->
