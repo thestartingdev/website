e4edc0574af881e900d9a20f564c773d8beee6be
jpromanonet <programming@jpromano.net>
2020-09-14 12:50:15 -0600

    MVC

--- views/exito.php
+++ views/exito.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <meta http-equiv="refresh" content="5;URL=./index.html" />
+  <meta http-equiv="refresh" content="5;URL=../index.php" />
 
   <title>The Starting Dev</title>
   <meta content="" name="descriptison">
@@ -31,36 +31,9 @@
 
 <body>
 
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top">
-    <div class="container d-flex align-items-center">
-      <h1 class="logo mr-auto"><a href="index.html">The Starting Dev</a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
-      <nav class="nav-menu d-none d-lg-block">
-        <ul>
-          <li><a href="./index.html">Inicio</a></li>
-          <li><a href="./acerca.html">Acerca de</a></li>
-          <li class="drop-down">
-            <a  href="#">Información</a>
-            <ul>
-              <li><a href="./consejos.html">Consejos Dev</a></li>
-              <li><a href="./recursos.html">Recursos gratis</a></li>
-              <li ><a href="./inscripcion.html">Inscripción</a></li>
-              <li><a href="./voluntario.html">Ser Voluntario</a></li>
-              <li><a href="./ideas.html">Aportar Ideas</a></li>
-              <li><a href="./conducta.html">Codigo de Conducta</a></li>
-              <li><a href="./roadmap.html">Roadmap / Programa</a></li>
-            </ul>
-          </li>
-          <li><a href="./contacto.html">Contacto</a></li>
-          <li><a href="/campus/"><b>Campus</b></a></li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-  <!-- End Header -->
+<?php
+    include("./modules/navbar.php");
+  ?>
 
   <main id="main">
 
@@ -88,46 +61,25 @@
 
   </main><!-- End #main -->
 
-<!-- ======= Footer ======= -->
-<footer id="footer">
-  <div class="footer-top">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-lg-6">
-          <h3>Seguinos en...</h3>
-        </div>
-      </div>
-
-      <div class="social-links">
-        <a href="https://twitter.com/thestartingdev" target="_blank" class="twitter"><i class="bx bxl-twitter"></i></a>
-        <a href="https://www.instagram.com/thestartingdev/" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
-        <a href="https://www.linkedin.com/company/the-starting-dev" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-        <a href="https://github.com/thestartingdev" target="_blank" class="github"><i class="bx bxl-github"></i></a>
-        <a href="https://www.youtube.com/channel/UC09pnxn0b-dVNS0rWotnWQQ" target="_blank" class="youtube"><i class="bx bxl-youtube"></i></a>
-      </div>
-      <br>
-      <br>
-      <h6><a href="./terminosCondiciones.html">Terminos y condiciones</a> | <a href="./politicaPrivacidad.html"> Politica de privacidad</a></h6>
-    </div>
-  </div>
-</footer>
-<!-- End Footer -->
+  <?php
+    include("./modules/footer.php")
+  ?>
 
   <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/jquery/jquery.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
-  <script src="assets/vendor/counterup/counterup.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/venobox/venobox.min.js"></script>
-  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="../assets/vendor/jquery/jquery.min.js"></script>
+  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="../assets/vendor/php-email-form/validate.js"></script>
+  <script src="../assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="../assets/vendor/counterup/counterup.min.js"></script>
+  <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="../assets/vendor/venobox/venobox.min.js"></script>
+  <script src="../assets/vendor/owl.carousel/owl.carousel.min.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="../assets/js/main.js"></script>
 
 </body>
 
